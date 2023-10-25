169b807d3c4cc325984dd9aaea5ec5ed22de0e2f
Mohammad <wintercodess@gmail.com>
2023-10-26 00:02:51 +0200

    Initial commit

---
+++ app/src/main/java/com/winter/noteapp/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.winter.noteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NoteAppOverview()
+        }
+    }
+}
+// Global variable to store the notes
+val notes = mutableStateListOf<Note>()
+
+class Note(val title: String, val description: String)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NoteAppOverview() {
+    Scaffold(
+        modifier = Modifier,
+        containerColor = MaterialTheme.colorScheme.background,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Note App")
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* Navigate to the note creation screen */ }
+            ) {
+                Text("+")
+            }
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            if (notes.isEmpty()) {
+                Text(text = "No notes yet!")
+            } else {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    items(notes) {note ->
+                        NoteItem(note)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NoteItem(note: Note) {
+    Card(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = note.title,
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Text(
+                text = note.description,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NoteCreateScreen(note: Note? = null) {
+    val titleTextState = remember { mutableStateOf(note?.title ?: "") }
+    val titledescriptionState = remember { mutableStateOf(note?.title ?: "") }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        if (note == null) "Create New Note"
+                        else "Edit Note"
+                    )
+                }
+            )},
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* Save the note */ },
+
+            ) {}
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            TextField(
+                value = titleTextState.value,
+                onValueChange = { titleTextState.value = it }, 
+                label = { Text("Title") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            TextField(
+                value = titledescriptionState.value,
+                onValueChange = { titledescriptionState.value = it },
+                label = { Text(text = "Desc") },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NotesOverviewScreenPreview() {
+    NoteAppOverview()
+}
---
+++ app/src/main/java/com/winter/noteapp/screens/StartScreen.kt
@@ -0,0 +1,2 @@
+package com.winter.noteapp.screens
+
